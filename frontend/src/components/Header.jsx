d921f2403f345736acd80a3164c1c1ec5560978b
Dervish Skenderi <dsk_95@hotmail.com>
2025-05-07 08:52:05 +0200

    Header and Footer components

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+            <Container>
+                <Navbar.Brand href="/">DSK</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="/cart"><FaShoppingCart> Cart</FaShoppingCart></Nav.Link>
+                        <Nav.Link href="/login"><FaUser> Sign In</FaUser></Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    </header>
+  )
+}
+
+export default Header
