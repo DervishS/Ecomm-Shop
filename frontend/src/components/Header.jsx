4e375fbcffefbd3ef843c0b923309622900ed090
Dervish Skenderi <dsk_95@hotmail.com>
2025-08-03 18:48:53 +0200

    Created Product, Rating, ProductScreen, HomeScreen, and other initial design commponents for frontend

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,21 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
-import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap';
+import React from 'react';
 
 const Header = () => {
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">DSK</Navbar.Brand>
+                <LinkContainer to="/">
+                <Navbar.Brand>DSK</Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="/cart"><FaShoppingCart> Cart</FaShoppingCart></Nav.Link>
-                        <Nav.Link href="/login"><FaUser> Sign In</FaUser></Nav.Link>
+                        <LinkContainer to="/cart"><Nav.Link><FaShoppingCart> Cart</FaShoppingCart></Nav.Link></LinkContainer>
+                        <LinkContainer to="/login"><Nav.Link><FaUser> Sign In</FaUser></Nav.Link></LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
