ee7253110026632d3134c24a883f47c276d252fe
Dervish Skenderi <dsk_95@hotmail.com>
2025-08-17 17:18:46 +0200

    I created a connection between the app and Mongo DB Atlas I also created the first schemas for: Users, Products, and Orders.

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 
 const port = process.env.PORT || 8000;
+
+connectDB(); //Connecting to the Mongo DB.
 
 const app = express();
 
