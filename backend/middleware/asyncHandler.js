986c265e7b53b834bef3932bfa042207d408c928
Dervish Skenderi <dsk_95@hotmail.com>
2025-08-19 08:48:29 +0200

    Added errorMiddleware.js to handle 404 and 500 errors.

--- backend/middleware/asyncHandler.js
+++ backend/middleware/asyncHandler.js
@@ -1,5 +1,6 @@
 const asyncHandler = fn => (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next);
 }
-
+// the idea is to avoid the overhead of try catch block or when using
+// functions that return promises.
 export default asyncHandler;
