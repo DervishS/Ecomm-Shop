e034b558a1ee520c528586aa96aecfd8b0482a25
Dervish Skenderi <dsk_95@hotmail.com>
2025-09-03 08:59:32 +0200

    Adding changes I performed on the 24th August, 2nd September, and Today.

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -3,28 +3,19 @@ import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import Rating from '../components/Rating';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-
+import { useGetProductDetailsQuery } from '../slices/productsApiSlice.js';
+import Loader from '../components/Loader.jsx';
+import Message from '../components/Message.jsx';
 
 const ProductScreen = () => {
-    const [product, setProduct] = useState([]);
-
     const { id: productId } = useParams();
-
-    useEffect(() => {
-        const fetchProduct = async () => {
-            const { data } = await axios.get(`/api/products/${productId}`);
-            setProduct(data);
-        }
-
-        fetchProduct();
-    }, [productId]);
     
-
+    const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
   return <>
     <Link className='btn btn-light my-3' to="/">Go Back</Link>
-    <Row>
+    {isLoading ? (
+        <Loader />
+    ) : error ? (<Message variant='danger'> {error?.data?.message || error.error}</Message>) : (<Row>
         <Col md={5}>
             <Image src={product.image} alt={product.name} fluid />
         </Col>
@@ -69,7 +60,7 @@ const ProductScreen = () => {
                 </ListGroup>
             </Card>
         </Col>
-    </Row>
+    </Row>) }   
   </>
 };
 export default ProductScreen;
