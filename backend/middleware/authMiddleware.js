4121a8607aa48d9c95212aad1b03abffbddd3be6
Dervish Skenderi <dsk_95@hotmail.com>
2025-09-08 10:10:49 +0200

    Installed JWT Token, added it to the userController.js. Created a authMiddleware and also added cookie-parser.
    Added protected and admin routes in user Routes.

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,39 @@
+import jwt from 'jsonwebtoken';
+import asyncHandler from './asyncHandler.js';
+import User from '../models/userModel.js';
+
+// Protect routes
+const protect = asyncHandler(async (req, res, next) => {
+    let token;
+
+    // read the JWT token from the cookie
+    token = req.cookies.jwt;
+
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            req.user = await User.findById(decoded.userId).select('-password');
+            next();
+        } catch (error) {
+                console.log(error);
+                res.status(401);
+                throw new Error('Not Authorized, token failed');
+        }
+    } else {
+        res.status(401);
+        throw new Error('Not Authorized, no token')
+    }
+});
+
+// ADMIN Middleware
+const admin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        next();
+    } else {
+        res.status(401);
+        throw new Error('User is not athuorized as a Admin user')
+    }
+};
+
+export { protect, admin};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,12 +11,13 @@ import {
     getUsersByID,
     updateUser,
  } from "../controllers/userController.js";
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 //api/users
-router.route('/').post(registerUser).get(getUsers);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/logout', logoutUser);
 router.post('/login', authUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
-router.route('/:id').delete(deleteUser).get(getUsersByID).put(updateUser)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+router.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUsersByID).put(protect, admin, updateUser)
 
 export default router;
