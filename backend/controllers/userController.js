4a2add78c7f48d98eb64cec28cf3e59120febb77
Dervish Skenderi <dsk_95@hotmail.com>
2025-09-08 09:01:55 +0200

    Backend:
    Created userController
    
    Created userRoutes
    
    added user routes to server.js
    
    Tested all routes in POSTMAN

---
+++ backend/controllers/userController.js
@@ -0,0 +1,77 @@
+import asyncHandler from '../middleware/asyncHandler.js';
+import User from '../models/userModel.js';
+
+// @desc Auth user & get token
+// @route POST /api/users/login
+// @access Public
+const authUser = asyncHandler (async (req, res) => {
+    res.send('auth user');
+});
+
+// @desc Register user
+// @route GET /api/users
+// @access Public
+const registerUser = asyncHandler (async (req, res) => {
+    res.send('register user');
+});
+
+// @desc Logout user & clear cookie
+// @route POST /api/users/logout
+// @access Private
+const logoutUser = asyncHandler (async (req, res) => {
+    res.send('Logout user');
+});
+
+// @desc Get user profile
+// @route GET /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler (async (req, res) => {
+    res.send('Get user profile');
+});
+
+// @desc Update user profile
+// @route PUT /api/users/profile (we don't have ID because we use Token)
+// @access Private
+const updateUserProfile = asyncHandler (async (req, res) => {
+    res.send('Update user profile');
+});
+
+// @desc Get all user profile
+// @route GET /api/users
+// @access Private/Admin
+const getUsers = asyncHandler (async (req, res) => {
+    res.send('Get all user profiles');
+});
+
+// @desc Get user by ID
+// @route GET /api/users
+// @access Private/Admin
+const getUsersByID = asyncHandler (async (req, res) => {
+    res.send('Get specific user profile');
+});
+
+// @desc Delete user profile
+// @route DELETE /api/users/:id
+// @access Private/Admin
+const deleteUser = asyncHandler (async (req, res) => {
+    res.send('Delete user');
+});
+
+// @desc Update user profile
+// @route PUT /api/users/:id
+// @access Private/Admin
+const updateUser = asyncHandler (async (req, res) => {
+    res.send('Update user');
+});
+
+export { 
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile,
+    getUsers,
+    deleteUser,
+    getUsersByID,
+    updateUser,
+};
