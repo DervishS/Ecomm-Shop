a126e5a88dca80bd2e1042285047a358c3b72b8b
Dervish Skenderi <dsk_95@hotmail.com>
2025-09-26 08:38:51 +0200

    Backend & Frontend:
    Implement user management features: add delete user functionality, update user routes, and create user list screen

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -125,7 +125,7 @@ const updateUserProfile = asyncHandler (async (req, res) => {
 // @access Private/Admin
 const getUsers = asyncHandler (async (req, res) => {
     const users = await User.find({ });
-    res.status(200).res.json(users);
+    res.status(200).json(users);
 });
 
 // @desc Get user by ID
@@ -136,30 +136,6 @@ const getUsersByID = asyncHandler (async (req, res) => {
 
     if (user) {
         res.status(200).res.json(user);
-    } else {
-        res.status(404);
-        throw new Error('User not found!');
-    }
-});
-
-// @desc Update user profile
-// @route PUT /api/users/:id
-// @access Private/Admin
-const updateUser = asyncHandler (async (req, res) => {
-    const user = await User.findById(req.params.id);
-
-    if (user) {
-        user.name = req.body.name || user.name;
-        user.email = req.body.email || user.email;
-        user.isAdmin = Boolean(req.body.isAdmin);
-
-        const updateUser = await user.save();
-        res.status(200).res.json({
-            _id: updateUser._id,
-            name: updateUser.name,
-            email: updateUser.email,
-            isAdmin: updateUser.isAdmin,
-        });
     } else {
         res.status(404);
         throw new Error('User not found!');
@@ -178,7 +154,31 @@ const deleteUser = asyncHandler (async (req, res) => {
             throw new Error('Can not delete admin user');
         }
         await User.deleteOne({ _id: user._id });
-        res.status(200).json({ message: 'User removed' });
+        res.json({ message: 'User removed' });
+    } else {
+        res.status(404);
+        throw new Error('User not found!');
+    }
+});
+
+// @desc Update user profile
+// @route PUT /api/users/:id
+// @access Private/Admin
+const updateUser = asyncHandler (async (req, res) => {
+    const user = await User.findById(req.params.id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        user.isAdmin = Boolean(req.body.isAdmin);
+
+        const updateUser = await user.save();
+        res.json({
+            _id: updateUser._id,
+            name: updateUser.name,
+            email: updateUser.email,
+            isAdmin: updateUser.isAdmin,
+        });
     } else {
         res.status(404);
         throw new Error('User not found!');
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -14,10 +14,16 @@ import {
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 //api/users
-router.route('/').post(registerUser).get(protect, admin, getUsers);
+router.route('/').post(registerUser)
+    .get(protect, admin, getUsers);
 router.post('/logout', logoutUser);
 router.post('/login', authUser);
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
-router.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUsersByID).put(protect, admin, updateUser)
+router.route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile);
+router.route('/:id')
+    .delete(protect, admin, deleteUser)
+    .get(protect, admin, getUsersByID)
+    .put(protect, admin, updateUser)
 
 export default router;
