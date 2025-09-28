0edff5f1a60c51f32aad408d5c6a86e2dcc553ef
Dervish Skenderi <dsk_95@hotmail.com>
2025-09-28 16:55:51 +0200

    Bakend: added logic for pagination on the getProducts controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,8 +6,13 @@ import Product from '../models/productModel.js';
 // @access Public
 
 const getProducts = asyncHandler (async (req, res) => {
-    const products = await Product.find({  }); // empty object to get all of them
-    res.json(products);
+    const pageSize = 2;
+
+    const page = Number(req.query.pageNumber) || 1;
+    const count = await Product.countDocuments();
+
+    const products = (await Product.find({})).limit(pageSize).skip(pageSize * (page - 1)); // empty object to get all of them
+    res.json({products, page, pages: Math.ceil(count / pageSize)});
 });
 
 // @desc Fetch a single product by Id
