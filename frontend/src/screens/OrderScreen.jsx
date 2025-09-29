a6dd3796ac5d474aafceaa4bf0cac8f38104e5b3
Dervish Skenderi <dsk_95@hotmail.com>
2025-09-29 19:45:43 +0200

    Frontend:
    Fixed some Message components, specifically the error output.

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -101,7 +101,7 @@ const OrderScreen = () => {
         }
     };
 
-    return isLoading ? <Loader /> : error ? <Message variant='danger' /> : 
+    return isLoading ? <Loader /> : error ? <Message variant='danger'>{error?.data?.message || error.message}</Message> : 
     (
         <><h1>Order {order._id}</h1>
         <Row>
