32b7453a83bbe46638dbeb47776b46575674dd6d
Dervish Skenderi <dsk_95@hotmail.com>
2025-09-29 19:50:51 +0200

    Frontend:
    Added logic to properly reset user information and cart contents.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,6 +6,7 @@ import { useLogoutMutation } from '../slices/usersApiSlice';
 import { logout } from '../slices/authSlice';
 import { useNavigate } from 'react-router-dom';
 import SearchBox from './SearchBox';
+import { resetCart } from '../slices/cartSlice';
 
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
@@ -19,6 +20,7 @@ const Header = () => {
     try {
       await logoutApiCall().unwrap();
       dispatch(logout());
+      dispatch(resetCart());
       navigate('/login');
 
     } catch (err) {
