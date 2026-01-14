a077ef2a2ed3da317c37d2a39c4d7834c2312738
Dervish Skenderi <dsk_95@hotmail.com>
2026-01-14 23:17:56 +0100

    Add new image file 'woods-dusk.jpg' to uploads directory

--- mobile-frontend/app/(dashboard)/products.jsx
+++ mobile-frontend/app/(dashboard)/products.jsx
@@ -44,14 +44,17 @@ const products = () => {
         <Spacer />
         <FlatList 
             data={products} 
-            keyExtractor={(item) => item._id} 
+            keyExtractor={(item) => item._id}
+            numColumns={2}
+            contentContainerStyle={{ padding: 10 }}
+            columnWrapperStyle={{ justifyContent: 'space-between' }}
             renderItem={({ item }) => (
-            <ThemedView style={{ padding: 10, borderBottomWidth: 1, borderBottomColor: '#ccc', width: '100%' }}>
-                <Image source={{ uri: `${BASE_URL}/images/${item.image}` }} style={{ width: '100%', height: 200, resizeMode: 'cover' }} />
-                <ThemedText style={{ fontSize: 18, fontWeight: 'bold' }}>{item.name}</ThemedText>
-                <ThemedText style={{ fontSize: 16 }}>${item.price.toFixed(2)}</ThemedText>
-                <Spacer height={5} />
-                <ThemedButton title="View Details" onPress={() => { /* Navigate to product details */ }} />
+            <ThemedView style={styles.card}>
+                <Image source={{ uri: `${BASE_URL}/uploads/${item.image.replace('/images/', '')}` }} style={[styles.image, { resizeMode: 'contain' }]} />
+                <ThemedText style={[styles.name, { flex: 1 }]}>{item.name}</ThemedText>
+                <ThemedText style={styles.price}>${item.price.toFixed(2)}</ThemedText>
+                <Spacer height={10} />
+                <ThemedButton title="View Details" onPress={() => { /* Navigate to product details */ }}><ThemedText style={{ textAlign: 'center' }}>Details</ThemedText></ThemedButton>
             </ThemedView>
         )} />   
     </ThemedView>
@@ -72,5 +75,25 @@ const styles = StyleSheet.create({
         left: 20,
         width: 100,
         height: 100,
-    }
+    },
+    image: {
+        width: '100%',
+        aspectRatio: 1,
+        marginBottom: 10,
+        borderRadius: 12,
+    },
+    card: {
+        width: '48%',
+        borderRadius: 8,
+        marginBottom: 15,
+        padding: 10,
+    },
+    name: {
+        fontSize: 14,
+        fontWeight: '500',
+    },
+    price: {
+        fontSize: 13,
+        fontWeight: '500',
+    }, 
 })
